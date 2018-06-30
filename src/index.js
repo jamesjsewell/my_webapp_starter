046b48fdd9eedcacc0f3d5a4b7399ffa655ed269
james sewell <jamesjosephsewell@gmail.com>
2018-06-30 13:15:09 -0500

    build script is building semantic ui for react

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ import { Container, Segment } from 'semantic-ui-react'
 
 
 const Index = () => {
-    return <Container><Segment><div className="hello">hello</div></Segment></Container>
+    return (<Container><Segment.Group>
+                <Segment>Top</Segment>
+                <Segment.Group>
+                    <Segment>Nested Top</Segment>
+                    <Segment>Nested Middle</Segment>
+                    <Segment>Nested Bottom</Segment>
+                </Segment.Group>
+                <Segment.Group horizontal>
+                    <Segment>Top</Segment>
+                    <Segment>Middle</Segment>
+                    <Segment>Bottom</Segment>
+                </Segment.Group>
+                    <Segment>Bottom</Segment>
+            </Segment.Group></Container>)
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
