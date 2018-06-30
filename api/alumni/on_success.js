3c0d848143f9b96dd696ed7854d251cdf8ccf4fa
james sewell <jamesjosephsewell@gmail.com>
2018-06-30 15:26:01 -0500

    removed api

--- api/alumni/on_success.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // takes the request from the http request, the response object, message to client, data output
-    // returns it back to the client
-    success: function(req, res, msg, output){
-        res.status(201).json(output)
-    }
-}
