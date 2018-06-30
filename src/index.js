f107f19484108c950ebcacb2c9774be10ac4478d
james sewell <jamesjosephsewell@gmail.com>
2018-06-30 11:26:22 -0500

    webpack is working

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './style.scss';
+
+const Index = () => {
+    return <div className="hello">hello</div>
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
+
